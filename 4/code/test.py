bd67e6af5adff3cd91b9714aef31b95a87c714e8
wuyuqi <270738671@qq.com>
2018-11-05 23:39:36 +0800

    update

--- 4/code/test.py
+++ 4/code/test.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
-img = cv2.imread("lab1.bmp")
-l,a,b = cv2.split(img)
+img = cv2.imread("../teatdata/3.bmp")
+l,a,b=cv2.split(img)
+
 x = np.array(l)
 y = np.array(a)
 z = np.array(b)
 
-print(np.mean(a))
+print(np.mean())
 print(np.mean(x))
 print(np.mean(y))
